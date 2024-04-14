9ce75a9885b02ff63c2d16803ad3ed5906696a96
mustafa sen <mustafasen@tuta.io>
2024-04-14 15:57:14 +0300

    minor fixes

--- src/index.ts
+++ src/index.ts
@@ -4,23 +4,38 @@ import { join } from "node:path";
 
 const DIR_2_ROUTE_ROOT = process.env.DIR_2_ROUTE_ROOT || "public";
 
-const pageRoutes: {} | {[key: string]: (req: IncomingMessage, res: ServerResponse<IncomingMessage>) => void} = {};
+const pageRoutes:
+  | {}
+  | {
+      [key: string]: (
+        req: IncomingMessage,
+        res: ServerResponse<IncomingMessage>
+      ) => void;
+    } = {};
 async function addPagePath(route: string, path: string) {
   if (route === "") route = "/";
   const document = await readFile(path, "utf-8");
-  pageRoutes[route] = (req: IncomingMessage, res: ServerResponse<IncomingMessage>) => {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(document); 
+  pageRoutes[route] = {
+    GET: (
+      req: IncomingMessage,
+      res: ServerResponse<IncomingMessage>
+    ) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(document);
+    }
   }
 }
 
 async function dirCrawler(currDir: string) {
   const pagePath = join(currDir, "index.html");
-  
+
   // check if index.html exists
   // if it does add it to the pages object
   if ((await stat(pagePath)).isFile()) {
-    addPagePath(currDir.split(DIR_2_ROUTE_ROOT)[1].replace(/\$/g, ""), pagePath);
+    addPagePath(
+      currDir.split(DIR_2_ROUTE_ROOT)[1].replace(/\$/g, ""),
+      pagePath
+    );
   }
 
   // filter entities starts with $ sign
@@ -44,6 +59,6 @@ export async function dir2route() {
   if ((await stat(pageRoutesDir)).isDirectory()) {
     await dirCrawler(pageRoutesDir);
   }
-  
-  return pageRoutes
+
+  return pageRoutes;
 }
